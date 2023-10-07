e93de58d483ad82a9e0443e16863b0ce2744f061
Vitaly Tolmachev <vitalhare@outlook.com>
2023-10-07 21:04:06 +0500

    Task3

---
+++ Tyuiu.TolmachevVV.CS.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolmachevVV.CS.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.TolmachevVV.CS.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void DistCheck()
+        {
+            DataService service1 = new DataService();
+
+            Assert.AreEqual(9.333, service1.DistanceLength(120, 0.077775));
+        }
+    }
+}
