0e0e424892c9179bc0f28afd17a686740de6923c
Vitaly Tolmachev <vitalhare@outlook.com>
2023-10-12 19:33:55 +0500

    Task5

---
+++ Tyuiu.TolmachevVV.CS.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolmachevVV.CS.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.TolmachevVV.CS.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void AngelToHours()
+        {
+            DataService service1 = new DataService();
+            Assert.AreEqual(8, service1.AngleToHoursMinutes(240.123));
+        }
+    }
+}
